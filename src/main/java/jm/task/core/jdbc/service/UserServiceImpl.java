f48fd5dd7c6ab77ab415b97fbd2a407bb1764c8a
sihes <siheshteger@gmail.com>
2022-12-28 18:59:08 +0300

    JDBC

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -6,39 +6,39 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    //private final UserDaoJDBCImpl userDaoJDBC;
-    private final UserDaoHibernateImpl userDaoHibernate;
+    private final UserDaoJDBCImpl userDaoJDBC;
+    //private final UserDaoJDBCImpl userDaoHibernate;
     public UserServiceImpl() {
-        //this.userDaoJDBC = new UserDaoJDBCImpl();
-        this.userDaoHibernate = new UserDaoHibernateImpl();
+        this.userDaoJDBC = new UserDaoJDBCImpl();
+        //this.userDaoHibernate = new UserDaoHibernateImpl();
     }
     public void createUsersTable() {
-        //this.userDaoJDBC.createUsersTable();
-        this.userDaoHibernate.createUsersTable();
+        this.userDaoJDBC.createUsersTable();
+        //this.userDaoHibernate.createUsersTable();
     }
 
     public void dropUsersTable() {
-        //this.userDaoJDBC.dropUsersTable();
-        this.userDaoHibernate.dropUsersTable();
+        this.userDaoJDBC.dropUsersTable();
+        //this.userDaoHibernate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        //this.userDaoJDBC.saveUser(name, lastName, age);
-        this.userDaoHibernate.saveUser(name, lastName, age);
+        this.userDaoJDBC.saveUser(name, lastName, age);
+        //this.userDaoHibernate.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        //this.userDaoJDBC.removeUserById(id);
-        this.userDaoHibernate.removeUserById(id);
+        this.userDaoJDBC.removeUserById(id);
+        //this.userDaoHibernate.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        //return userDaoJDBC.getAllUsers();
-        return this.userDaoHibernate.getAllUsers();
+        return userDaoJDBC.getAllUsers();
+        //return this.userDaoHibernate.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        //this.userDaoJDBC.cleanUsersTable();
-        this.userDaoHibernate.cleanUsersTable();
+        this.userDaoJDBC.cleanUsersTable();
+        //this.userDaoHibernate.cleanUsersTable();
     }
 }
